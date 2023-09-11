44d800c22f9fb7d5f5654b0398cd4da8bce1f6d9
IsaacAndra <isaac.andra84@gmail.com>
2023-09-10 22:56:29 -0300

    :sparkles: feat: Added the HTTP Method "POST" (sendTask)

--- src/main/java/task/manager/api/controller/TaskController.java
+++ src/main/java/task/manager/api/controller/TaskController.java
@@ -1,12 +1,36 @@
 package task.manager.api.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import task.manager.api.domain.task.Task;
+import task.manager.api.domain.task.TaskDetailingData;
+import task.manager.api.domain.task.TaskRegisterData;
+import task.manager.api.domain.task.TaskRepository;
 
 @RequestMapping("tasks")
 @RestController
 public class TaskController {
 
+    @Autowired
+    private TaskRepository repository;
 
+    @PostMapping
+    @Transactional
+    public ResponseEntity sendTask(@RequestBody @Valid TaskRegisterData data, UriComponentsBuilder uriBuilder){
+        var task = new Task(data);
+        repository.save(task);
+
+        var uri = uriBuilder.path("/tasks/{id}").buildAndExpand(task.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new TaskDetailingData(task));
+
+    }
 
 }
