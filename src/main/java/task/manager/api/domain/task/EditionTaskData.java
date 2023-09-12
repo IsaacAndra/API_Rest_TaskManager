f6998536afebab05572ce701a839ae08ed11199e
IsaacAndra <isaac.andra84@gmail.com>
2023-09-12 20:08:05 -0300

    :sparkles: feat: Added task edition dto

---
+++ src/main/java/task/manager/api/domain/task/EditionTaskData.java
@@ -0,0 +1,19 @@
+package task.manager.api.domain.task;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record EditionTaskData(
+        @NotNull
+        Long id,
+        String title,
+        String description,
+        @NotNull
+        LocalDateTime duedate,
+        @NotNull
+        Boolean completed
+
+) {
+
+}
