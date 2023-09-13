2daee778ae1d42170a81e62958d5ce5e57b3647b
IsaacAndra <isaac.andra84@gmail.com>
2023-09-12 21:18:27 -0300

    :sparkles: feat: Added the HTTP method "GET" to list the incompleted tasks

--- src/main/java/task/manager/api/controller/TaskController.java
+++ src/main/java/task/manager/api/controller/TaskController.java
@@ -2,6 +2,9 @@ package task.manager.api.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -35,6 +38,12 @@ public class TaskController {
         task.updateTask(data);
 
         return ResponseEntity.ok(new TaskDetailingData(task));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ListTaskData>> listTask(@PageableDefault(size = 10, sort = {"id"}) Pageable pagination){
+        var page = repository.findAllByCompletedFalse(pagination).map(ListTaskData::new);
+        return ResponseEntity.ok(page);
     }
 
 }
