b47158a65d6ba30423b0a638ed60bf713d71385b
IsaacAndra <isaac.andra84@gmail.com>
2023-09-16 20:29:55 -0300

    :sparkles: feat: Added a method HTTP delete

--- src/main/java/task/manager/api/controller/TaskController.java
+++ src/main/java/task/manager/api/controller/TaskController.java
@@ -18,32 +18,47 @@ public class TaskController {
     @Autowired
     private TaskRepository repository;
 
+    @GetMapping
+    public ResponseEntity<Page<ListTaskData>> listIncompletedTask(@PageableDefault(size = 10, sort = {"id"}) Pageable pagination){
+        var page = repository.findAllByCompletedFalse(pagination).map(ListTaskData::new);
+
+        return ResponseEntity.ok(page);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity listIncompletedById(@PathVariable Long id){
+        var task = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new TaskDetailingData(task));
+    }
+
     @PostMapping
     @Transactional
     public ResponseEntity sendTask(@RequestBody @Valid TaskRegisterData data, UriComponentsBuilder uriBuilder){
         var task = new Task(data);
         repository.save(task);
-
         var uri = uriBuilder.path("/tasks/{id}").buildAndExpand(task.getId()).toUri();
 
         return ResponseEntity.created(uri).body(new TaskDetailingData(task));
-
     }
 
     @PutMapping
     @Transactional
     public ResponseEntity editTask(@RequestBody @Valid EditionTaskData data){
         var task = repository.getReferenceById(data.id());
-
         task.updateTask(data);
 
         return ResponseEntity.ok(new TaskDetailingData(task));
     }
 
-    @GetMapping
-    public ResponseEntity<Page<ListTaskData>> listTask(@PageableDefault(size = 10, sort = {"id"}) Pageable pagination){
-        var page = repository.findAllByCompletedFalse(pagination).map(ListTaskData::new);
-        return ResponseEntity.ok(page);
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteTas(@PathVariable Long id){
+        var task = repository.getReferenceById(id);
+        task.delete();
+
+        return ResponseEntity.noContent().build();
     }
 
 }
